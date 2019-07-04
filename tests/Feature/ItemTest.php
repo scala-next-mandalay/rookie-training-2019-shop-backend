54893c19f377e408ae124ba36db4d653e8c96aa4
Kay Thwe Aung <kaythweaung@scala-next.com>
2019-07-04 15:50:56 +0630

    Prepared fix testfunction
    	I am tryping prepare test function for item in shopbackend.

--- tests/Feature/ItemTest.php
+++ tests/Feature/ItemTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Item;
+use App\Models\Category;
 
 class ItemTest extends TestCase
 {
@@ -21,14 +23,14 @@ class ItemTest extends TestCase
         $response->assertStatus(200);
     }
 
-
+     /** @test */
     public function on_index_items_success()
     {
         $category =  factory(Category::class)->create();
         $exps = factory(Item::class, 2)->create(['category_id' => $category->id]);
-        
+        echo "Item index .....";
         $now = time();
-        $res = $this->json('GET', '/api/items?offset=10'); 
+        $res = $this->json('GET', '/api/items?offset=0'); 
         $res->assertStatus(200); 
         $res->assertExactJson([
             'data' => [
@@ -70,9 +72,10 @@ class ItemTest extends TestCase
         ]);
     }
 
-
+     /** @test */
     public function items_are_order_by_id_desc()
     {
+        echo "Item Desc .....";
         factory(Item::class)->create(['id' => 8]);
         factory(Item::class)->create(['id' => 35]);
         factory(Item::class)->create(['id' => 1250]);
@@ -88,8 +91,10 @@ class ItemTest extends TestCase
         ]);
     }
 
+     /** @test */
     public function deleted_items_are_not_shown()
     {
+        echo "Not show deleted item ....";
         $row1 = factory(Item::class)->create();
         $row2 = factory(Item::class)->create();
         $row2->delete();
@@ -101,14 +106,16 @@ class ItemTest extends TestCase
         $res->assertJsonCount(2, 'data');
         $res->assertJson([
             'data' => [
-                ['id' => $row1->id],
                 ['id' => $row3->id],
+                ['id' => $row1->id],
             ]
         ]);
     }
 
+     /** @test */
     public function get_11th_to_20th_items_if_limit10_offset10_totalSize30()
     {
+            echo "Item Offset .....";
            $category =  factory(Category::class)->create();
            $exps = factory(Item::class, 30)->create(['category_id' => $category->id]);
 
@@ -116,16 +123,16 @@ class ItemTest extends TestCase
            $res->assertJsonCount(10, 'data');
            $res->assertJson([
                'data' => [
-                   ['id' => $exps[10]->id],//11th
-                   ['id' => $exps[11]->id],
-                   ['id' => $exps[12]->id],
-                   ['id' => $exps[13]->id],
-                   ['id' => $exps[14]->id],
-                   ['id' => $exps[15]->id],
-                   ['id' => $exps[16]->id],
-                   ['id' => $exps[17]->id],
+                   ['id' => $exps[19]->id],//11th
                    ['id' => $exps[18]->id],
-                   ['id' => $exps[19]->id],//20th
+                   ['id' => $exps[17]->id],
+                   ['id' => $exps[16]->id],
+                   ['id' => $exps[15]->id],
+                   ['id' => $exps[14]->id],
+                   ['id' => $exps[13]->id],
+                   ['id' => $exps[12]->id],
+                   ['id' => $exps[11]->id],
+                   ['id' => $exps[10]->id],//20th
                ]
            ]);
      }
