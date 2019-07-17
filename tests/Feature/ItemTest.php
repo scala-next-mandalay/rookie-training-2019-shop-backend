387e1553d69f57fd50c6b5bb962c0f255b9b87cc
Kay Thwe Aung <kaythweaung@scala-next.com>
2019-07-17 16:18:21 +0630

    Again Push Nullable

--- tests/Feature/ItemTest.php
+++ tests/Feature/ItemTest.php
@@ -290,16 +290,17 @@ class ItemTest extends TestCase
     }    
 
      /** @test */
-    public function store_image_length_0_will_occur_validation_error()
+    public function store_image_length_0_will_no_validation_error()
     {
         $category =  factory(Category::class)->create();
-        $this->expectException(ValidationException::class);
+        // $this->expectException(ValidationException::class);
         $res = $this->json('POST', self::API_PATH, [
             'name' => 'item1',
             'price' => 999,
             'image' => '',
             'category_id' => $category->id
         ]);
+         $res->assertStatus(201);
     } 
 
     /** @test */
